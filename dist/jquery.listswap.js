b7eb2c7998eb4169b967e45d7f0c50c4784ce3d0
Berkan Dirim <berkandirim@gmail.com>
2017-05-04 14:29:01 +0300

    Module definitions

--- dist/jquery.listswap.js
+++ dist/jquery.listswap.js
@@ -1,11 +1,18 @@
 /*!
  * jQuery Listswap Plugin
- * Original author: @phedde
- * Changes: @berkandirim
+ * Author: @berkandirim
  * Licensed under the MIT license
  */
 
-;(function($, window, document, undefined) {
+;(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module depending on jQuery.
+        define(['jquery'], factory);
+    } else {
+        // No AMD. Register plugin with global jQuery object.
+        factory(jQuery);
+    }
+}(function($, window, document, undefined) {
     
     'use strict';
 
@@ -174,7 +181,7 @@
                 var val = $(this).val().toString().toLowerCase();
                 $(selector + ' > li').each(function() {
                     var text = $(this).text().toString().toLowerCase();
-                    if (text.indexOf(val) != -1)  $(this).show(); else $(this).hide();
+                    if (text.indexOf(val) !== -1)  $(this).show(); else $(this).hide();
                 });
             });
             _this.clearButton(selector);
@@ -209,4 +216,4 @@
         }
     };
 
-})(jQuery, window, document);
+})(jQuery, window, document));
